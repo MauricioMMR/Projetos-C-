6cde24936b95146b4f89ef612d27de6333aa8c32
Maurício Miranda <159303718+MauricioMMR@users.noreply.github.com>
2024-10-23 15:12:42 -0300

    Add files via upload

---
+++ Projeto4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imprimir_Valores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] valores = new int[50];
+            int i, Valor = 0;
+
+            for (i = 0; i < 5; i++)
+            {
+                Console.Write($"Inforde o {i + 1}º Valor:");
+                valores[i] = int.Parse(Console.ReadLine());
+
+
+                Valor = Valor + valores[1];
+            }
+
+            for (i = 0; i < 50; i++)
+            {
+                if (valores[i] > 50)
+                {
+                    Console.WriteLine($"Valor Acima de 50: {valores[i]}");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
